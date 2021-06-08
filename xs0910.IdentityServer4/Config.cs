4e51d0ae3a4ab22f9dd1c557ebb8d770684c67db
陈晓涛 <13680093521@163.com>
2021-06-08 23:40:27 +0800

    Migration DB and SeedData

---
+++ xs0910.IdentityServer4/Config.cs
@@ -0,0 +1,115 @@
+﻿using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace xs0910.IdentityServer4
+{
+    public class Config
+    {
+
+        /// <summary>
+        /// Identity Resources
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),         // 必填：不然会报错
+                new IdentityResources.Profile(),
+                new IdentityResources.Email(),
+                new IdentityResource("roles","角色",new List<string>{ JwtClaimTypes.Role })
+            };
+        }
+
+        /// <summary>
+        /// API Scopes：指定 Authorization Server允许访问的API Scopes
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<ApiScope> GetApiScopes()
+        {
+            return new[]
+            {
+                new ApiScope("scope")
+            };
+        }
+
+        /// <summary>
+        /// API资源：Authorization Server保护了哪些 API 资源
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new ApiResource[]
+            {
+                new ApiResource("xs0910.Core.API","xs0910.Core.API")
+                {
+                    UserClaims = { JwtClaimTypes.Name, JwtClaimTypes.Role },
+                    Scopes = { "scope" },
+                    ApiSecrets = { new Secret("secret".Sha256()) }
+                }
+            };
+        }
+
+        /// <summary>
+        /// 客户端：哪些客户端 Client 应用可以使用这个Authorization Server
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<Client> GetClients()
+        {
+            return new Client[]
+            {
+                // 1. vue项目
+                new Client
+                {
+                    // 客户端Id
+                    ClientId = "vue.js",
+                    ClientName ="Vue Client",
+                    AllowedGrantTypes = GrantTypes.Implicit,
+                    AllowAccessTokensViaBrowser =true,
+                    // 跳转地址
+                    RedirectUris = { "http://localhost:6688/callback" },
+                    // 登出地址
+                    PostLogoutRedirectUris = { "http://localhost:6688" },
+                    // 允许跨域地址
+                    AllowedCorsOrigins = { "http://localhost:6688" },
+                    // Token时效
+                    AccessTokenLifetime = 3600,
+                    // 允许访问的 API 资源
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "roles",
+                        "scope"
+                    }
+                },
+
+                // 2.控制台客户端
+                new Client
+                {
+                    ClientId = "Console",
+                    // client 用来获取token的密钥
+                    ClientSecrets={ new Secret("secret".Sha256()) },
+                    AllowedGrantTypes = new List<string>()
+                    {
+                        GrantTypes.ResourceOwnerPassword.FirstOrDefault(),
+                        GrantTypes.ClientCredentials.FirstOrDefault()
+                    },
+                    AllowedScopes =new List<string>
+                    {
+                       IdentityServerConstants.StandardScopes.OpenId,
+                       IdentityServerConstants.StandardScopes.Profile,
+                       "scope"
+                    }
+                },
+            };
+
+        }
+
+    }
+}
---
+++ xs0910.IdentityServer4/Data/SeedData.cs
@@ -0,0 +1,130 @@
+﻿using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using xs0910.IdentityServer4.Models;
+
+namespace xs0910.IdentityServer4.Data
+{
+    /// <summary>
+    /// 种子数据
+    /// </summary>
+    public class SeedData
+    {
+        /// <summary>
+        /// 执行 dotnet run /seed
+        /// </summary>
+        /// <param name="serviceProvider"></param>
+        public static void EnsureSeedData(IServiceProvider serviceProvider)
+        {
+            Console.WriteLine("Seeding Database...");
+
+            using (var scope = serviceProvider.GetRequiredService<IServiceProvider>().CreateScope())
+            {
+                // 1. 迁移 PersistentGrantDbContext 上下文
+                scope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+                // 2. 迁移 ConfigurationDbContext 上下文                
+                var configurationContext = scope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+                configurationContext.Database.Migrate();
+                // 2.1 生成GetClients、GetIdentityResources、GetApiResources、GetApiScopes数据
+                EnsureConfigurationData(configurationContext);
+
+                // 3. 迁移 ApplicationDbContext 上下文
+                var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                context.Database.Migrate();
+
+                var userMgr = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+                var roleMgr = scope.ServiceProvider.GetRequiredService<RoleManager<ApplicationRole>>();
+
+                // 3.1 读取默认数据
+
+                // 3.2 遍历用户
+
+                // 3.3 遍历角色
+                
+            }
+
+            Console.WriteLine("Done Seeding Database");
+            Console.WriteLine();
+        }
+
+
+        /// <summary>
+        /// 生成GetClients、GetIdentityResources、GetApiResources、GetApiScopes数据
+        /// </summary>
+        /// <param name="context"></param>
+        public static void EnsureConfigurationData(ConfigurationDbContext context)
+        {
+            if (!context.Clients.Any())
+            {
+                Console.WriteLine("Clients Building");
+
+                foreach (var client in Config.GetClients().ToList())
+                {
+                    context.Clients.Add(client.ToEntity());
+                }
+                context.SaveChanges();
+
+                Console.WriteLine("Clients Generated successfully");
+            }
+            else
+            {
+                Console.WriteLine("Clients Already Exists");
+            }
+
+            if (!context.IdentityResources.Any())
+            {
+                Console.WriteLine("IdentityResources Building");
+
+                foreach (var resource in Config.GetIdentityResources().ToList())
+                {
+                    context.IdentityResources.Add(resource.ToEntity());
+                }
+
+                context.SaveChanges();
+                Console.WriteLine("IdentityResources Generated successfully");
+            }
+            else
+            {
+                Console.WriteLine("IdentityResources Already Exists");
+            }
+
+
+            if (!context.ApiResources.Any())
+            {
+                Console.WriteLine("ApiResources Building");
+                foreach (var resource in Config.GetApiResources().ToList())
+                {
+                    context.ApiResources.Add(resource.ToEntity());
+                }
+                context.SaveChanges();
+                Console.WriteLine("ApiResources Generated successfully");
+            }
+            else
+            {
+                Console.WriteLine("ApiResources Already Exists");
+            }
+
+
+            if (!context.ApiScopes.Any())
+            {
+                Console.WriteLine("ApiScopes Building");
+                foreach (var resource in Config.GetApiScopes().ToList())
+                {
+                    context.ApiScopes.Add(resource.ToEntity());
+                }
+                context.SaveChanges();
+                Console.WriteLine("ApiScopes Generated successfully");
+            }
+            else
+            {
+                Console.WriteLine("ApiScopes Already Exists");
+            }
+        }
+
+    }
+}
