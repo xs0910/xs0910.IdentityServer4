e563d501bef2531ec9d8bd2bb21e001e610d612b
涛 <997933096@qq.com>
2021-06-11 18:19:22 +0800

    role

---
+++ xs0910.IdentityServer4/Controllers/RoleInfo/RoleInfoController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using xs0910.IdentityServer4.Models;
+
+namespace IdentityServerHost.Quickstart.UI
+{
+    /// <summary>
+    /// 角色信息控制器
+    /// </summary>
+    public class RoleInfoController : BaseController
+    {
+        private readonly RoleManager<ApplicationRole> _roleManager;
+
+        public RoleInfoController(RoleManager<ApplicationRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        /// <summary>
+        /// Role Lists Page
+        /// </summary>
+        /// <param name="returnUrl"></param>
+        /// <returns></returns>
+        public IActionResult Index(string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            var roles = _roleManager.Roles.Where(r => !r.IsDeleted).OrderBy(r => r.OrderSort).ToList();
+            return View(roles);
+        }
+    }
+}
